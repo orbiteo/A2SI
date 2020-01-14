2439e93ceb4039f15b7dd2b45ffd7b3fe90b7eab
Orbiteo <eloise@orbiteo.com>
2020-01-14 17:37:53 +0100

    fin fichier extract devis, reste à déterminer les devis non traités et n'envoyer que ceux là

--- extractQuotes.php
+++ extractQuotes.php
@@ -11,105 +11,78 @@ $dateDeLUpdate = date('Y-m-d');
 $myFileQuotes = _PS_MODULE_DIR_.'/interfaceerp/exports/devis/quotes'.$dateDeLUpdate.'.csv';
 $fhQuotes = fopen($myFileQuotes, 'w') or die("impossible de créer le fichier");
 //Inscrire les en-tete du fichier csv
-$enteteOrders = ["Type", "ID Client", "ID quote", "Nom Client", "Email CLient", "ID Adresse Facturation", "Ligne 1 Adresse Facturation", "CP Adresse Facturation", "Ville Adresse Facturation", "ID Adresse Livraison", "Ligne 1 Adresse Livraison", "CP Adresse Livraison", "Ville Adresse Livraison", "Reference Produit", "Quantite", "Siren"];
-fputcsv($fhOrders, $enteteOrders);
+$enteteQuotes = ["Type", "ID Client", "ID quote", "Nom Client", "Email CLient", "Reference Produit", "Quantite", "Siren"];
+fputcsv($fhQuotes, $enteteQuotes);
 
 $orderReference = "";
 
-$arrayORDR = []; //Tableau recap de toutes les commandes
+$arrayQuotes = []; //Tableau recap de tous les devis
 
-$SQL = Db::getInstance()->executeS("SELECT id_quotes, id_customer, products 
+$SQL = Db::getInstance()->executeS("SELECT id_quote, id_customer, products
     FROM "._DB_PREFIX_."quotes");
 
-var_dump($SQL);
+$webService = new PrestaShopWebservice(PS_SHOP_PATH, PS_WS_AUTH_KEY, DEBUG);
 
-//sortir le xml orders
-/*$webService = new PrestaShopWebservice(PS_SHOP_PATH, PS_WS_AUTH_KEY, DEBUG);
-try {
-  $xml = $webService->get(array('resource' => 'orders'));
-  $totalOrders = $xml->orders->children();
-  //Faire une boucle pour sortir tous les id
-  foreach ($totalOrders as $order) {
-    $idOrder = $order->attributes();
-    // faire un appel API avec chaque id
-    $xml = $webService->get(array('url' => PS_SHOP_PATH.'/api/orders/'.$idOrder));
-    $orderDetails = $xml->children()->children();
-    // extraire les données dans des variables puis dans un tableau par id, puis dans un tableau de tous les id
-    $orderId = $orderDetails->id;
 
-    if($orderDetails->current_state != "5" && $orderDetails->current_state != "6") { // si l'état de la commande n'est ni livrée (5), ni annulée (6), on remplit le tableau:
-      $invoiceAddress = $orderDetails->id_address_invoice;
-      $deliveryAdress = $orderDetails->id_address_delivery;
-      $orderCustomerId = $orderDetails->id_customer;
-      $orderReference = $orderDetails->reference;
-      $orderTotalPaid = $orderDetails->total_paid_tax_excl;
-      $orderDate = $orderDetails->date_add;
-      $orderPaymentType = $orderDetails->payment;
-      $totalShipping = $orderDetails->total_shipping_tax_excl;
-      $idTransp = $orderDetails->id_carrier;
-      $payment = $orderDetails->payment;
-      $orderCustomerName = "";
-      $customerEmail = "";
-      $customerSiret = "";
-      $orderIdsProducts = "";
-      $orderQuantities = "";
 
-      $productsDetails = $orderDetails->associations->children()->children();
-      foreach ($productsDetails as $key) {
-        $quantityPerId = $key->product_quantity;
-        $productPrice = $key->product_price;
-        $productReference = $key->product_reference;
+for($i=0 ; $i<count($SQL) ; $i++) { // boucle qui scale les devis
 
-        $arrayDetailsOrder = array("L", "", $orderReference, "", "", "", "", "", "", "", "", "", "", $productReference, $quantityPerId, $productPrice, "", "", "", "", "", "");
-        array_push($arrayORDR, $arrayDetailsOrder);
-      }
+    $id_quote = $SQL[$i]['id_quote'];
+    $id_customer = $SQL[$i]['id_customer'];
+    $customerName = '';
+    $customerEmail = '';
+    $customerSiret = '';
 
-      $xml5 = $webService->get(array('url' => PS_SHOP_PATH.'/api/addresses/'.$invoiceAddress));
-      $addressFact = $xml5->children()->children();
-      $ligne1Fact = $addressFact->address1;
-      $cpFact = $addressFact->postcode;
-      $villeFact = $addressFact->city;
-
-      $xml6 = $webService->get(array('url' => PS_SHOP_PATH.'/api/addresses/'.$deliveryAdress));
-      $addressDeliv = $xml6->children()->children();
-      $ligne1Deliv = $addressDeliv->address1;
-      $cpDeliv = $addressDeliv->postcode;
-      $villeDeliv = $addressDeliv->city;
-      try {
-        $xml3 = $webService->get(array('url' => PS_SHOP_PATH.'/api/customers/'.$orderCustomerId));
-        $customerDetail = $xml3->children()->children();
-        $idCustomerDetail = $customerDetail->id;
-          $orderCustomerName = $customerDetail->firstname .' '.$customerDetail->lastname;
-          $customerEmail = $customerDetail->email;
-          $customerSiret = $customerDetail->siret;
-      }
-      catch (PrestaShopWebserviceException $e) {
+    try {
+        $xml = $webService->get(array('url' => PS_SHOP_PATH.'/api/customers/'.$id_customer)); // On va sortir chaque fiche client   
+        $customer = $xml->children()->children();
+        $customerName = $customer->lastname;
+        $customerEmail = $customer->email;
+        $customerSiret = $customer->siret;
+    }
+    catch (PrestaShopWebserviceException $e) { // si id_customer inexistant
         $trace = $e->getTrace();
-        if ($trace[0]['args'][0] == 404) { //Si id client inexistant 
-          $idCustomerDetail = "";
-          $orderCustomerName = "";
-          $customerEmail = "";
-          $customerSiret = "";
+        if ($trace[0]['args'][0] == 404){
+            $customerName = "Non trouvé";
+            $customerEmail = "Non trouvé";
+            $customerSiret = "non trouvé";
         }
         else if ($trace[0]['args'][0] == 401) echo 'Bad auth key';
         else echo $e->getMessage();
-      }
-        
-      //}
-      $arrayEnteteOrder = array("T", $idCustomerDetail, $orderReference, $orderCustomerName, $customerEmail, $invoiceAddress, $ligne1Fact, $cpFact, $villeFact, $deliveryAdress, $ligne1Deliv, $cpDeliv, $villeDeliv, "", "", "", $orderTotalPaid, $totalShipping, $idTransp, $orderDate, $payment, $customerSiret);
-      array_push($arrayORDR, $arrayEnteteOrder); // ajouter chaque tableau client au tableau général
     }
-  }
-  // remplir le fichier csv avec ces données avec la méthode fputcsv()
-  foreach ($arrayORDR as $fields) {
-    fputcsv($fhOrders, $fields);
-  }
-  fclose($fhOrders);
+
+    $arrayProductRow = unserialize($SQL[$i]['products']); // Sortir sous forme de tableau le format json de la table
+    $ids_product = array();
+    $quantities_product = array();
+    foreach($arrayProductRow as $details) { // pour chaque poroduit présent dans le devis on sort son id et sa quantité dans un tableau
+        array_push($ids_product, $details["id"]);
+        array_push($quantities_product, $details["quantity"]);
+    }
+    // Appeler la table products avec chaque id pour sortir leurs références et créer une ligne pour chaque
+    for($j=0 ; $j<count($ids_product) ; $j++) { // pour chaque id produit, on va chercher la ref produit
+        try {
+            $xml = $webService->get(array('url' => PS_SHOP_PATH.'/api/products/'.$ids_product[$j])); // On va sortir chaque fiche client   
+            $product = $xml->children()->children();
+            $productReference = $product->reference;
+        }
+        catch (PrestaShopWebserviceException $e) { // si id_product inexistant
+            $trace = $e->getTrace();
+            if ($trace[0]['args'][0] == 404){
+                $productReference = "Non trouvé";
+            }
+            else if ($trace[0]['args'][0] == 401) echo 'Bad auth key';
+            else echo $e->getMessage();
+        }
+        $arrayDetQuotes = array("L", $productReference, $quantities_product[$j]);
+        array_push($arrayQuotes, $arrayDetQuotes); // ajouter chaque tableau détail quotes au tableau général
+    }
+
+    $arrayEnteteQuotes = array("T", $id_customer, $id_quote, $customerName, $customerEmail, $customerSiret);
+    array_push($arrayQuotes, $arrayEnteteQuotes); // ajouter chaque tableau quotes au tableau général  
 }
-catch (PrestaShopWebserviceException $e) {
-    $trace = $e->getTrace();
-    if ($trace[0]['args'][0] == 404) echo 'Bad ID';
-    else if ($trace[0]['args'][0] == 401) echo 'Bad auth key';
-    else echo $e->getMessage();
-}*/
-/*** / FIN EXTRACT ORDERS ***/
+
+ // remplir le fichier csv avec ces données avec la méthode fputcsv()
+ foreach ($arrayQuotes as $fields) {
+    fputcsv($fhQuotes, $fields);
+  }
+  fclose($fhQuotes);
